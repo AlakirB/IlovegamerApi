393b8a3218cfe47a75ca16a236d5d3a0be8db79b
alakirb <lopinto.alexandre@gmail.com>
2023-11-21 09:34:34 +0100

    3 differents templates

--- src/Controller/ApiFileReaderController.php
+++ src/Controller/ApiFileReaderController.php
@@ -22,7 +22,7 @@ class ApiFileReaderController extends AbstractController
 
         $fileContent = file_get_contents($filePath);
 
-        return $this->render('api_file_reader/index.html.twig', [
+        return $this->render("api_file_reader/index.$fileFormat.twig", [
             'file_content' => $fileContent,
         ]);
     }
